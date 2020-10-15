93c201883c1b938d38d5a3fe4920084ccc61a3df
Jake Nemiroff <jakenemiroff@gmail.com>
2020-10-15 18:45:16 -0400

    add function to retrieve stock data from yahoo finance

--- portfolio_correlation_matrix.py
+++ portfolio_correlation_matrix.py
@@ -5,16 +5,33 @@ import pandas_datareader.data as web
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+import mplfinance as mpf
+import matplotlib.dates as mdates
+import os
 
 style.use('ggplot')
 
 startDate = dt.datetime(2020, 1, 1)
 endDate = dt.datetime(2020, 10, 15)
 
-df = web.DataReader('AAPL', 'yahoo', startDate, endDate)
+MY_STOCKS = ['AAPL', 'CHKP', 'CLOU',
+            'ILMN', 'JNJ', 'KXS.TO', 'MSFT', 'PG',
+            'SNPS', 'SPLK', 'SQ', 'TSM', 'VGK', 'VGT',
+            'VTV', 'WIX', 'AMZN', 'COST', 'HD', 'IEMG',
+            'RY', 'TCEHY', 'TOU.TO', 'WM', 'XBI']
 
-# df.to_csv('AAPL.csv')
+def getYahooData():
 
-# df = pd.read_csv('AAPL.csv', parse_dates = True, index_col = 0)
+    if not os.path.exists('stocksData'):
+        os.makedirs('stocksData')
 
-# print(df.head())
+    for ticker in MY_STOCKS:
+
+        if not os.path.exists('stocksData/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', startDate, endDate)
+            print(df.head())
+            df.reset_index(inplace=True)
+            df.set_index("Date", inplace=True)
+            df.to_csv('stocksData/{}.csv'.format(ticker))
+
+getYahooData()
